e357eda5b5f8da72816545ee8df6dc133f6b6def
longvb <longvb@longvbs-MacBook-Air.local>
2023-11-09 14:41:21 +0700

    remove unused functions

--- src/lib/translator.py
+++
@@ -1,6 +0,0 @@
-from deep_translator import GoogleTranslator
-
-# Use any translator you like, in this example GoogleTranslator
-translated = GoogleTranslator(source='auto', target='de').translate("keep it up, you are awesome")  # output -> Weiter so, du bist großartig
-
-print(translated)
